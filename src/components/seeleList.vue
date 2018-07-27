fa7ccd7aacf60f6bf647c39c497b6918e70a78fc
sky-miao <mengmiao_sky@126.com>
2018-07-27 17:54:30 +0800

    list height (#13)
    * list height
    
    * min-height
    
    * min-height change

--- src/components/seeleList.vue
+++ src/components/seeleList.vue
@@ -186,7 +186,7 @@ export default {
     font-size: 18px;
   }
   .seeleList {
-    min-height: 500px;
+    min-height: 400px;
     background: #08274f;
     background: url("../assets/imgs/bg_04.png") no-repeat;
     background-size: 100% auto;
@@ -344,11 +344,6 @@ export default {
         }
       }
     }
-  }
-}
-@media screen and (min-width: 768px) {
-  .seeleList{
-    min-height: 950px;
   }
 }
 </style>
